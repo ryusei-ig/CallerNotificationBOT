3ed1f75414352f3b9dae5ce3eacae482d10cb8e9
ryusei-ig <42218382+ryusei-ig@users.noreply.github.com>
2018-12-20 19:01:48 +0900

    Update CallNotificationBOT.js

--- src/CallNotificationBOT.js
+++ src/CallNotificationBOT.js
@@ -25,7 +25,7 @@ function formatDate(date, format) {
   format = format.replace(/YYYY/g, date.getFullYear());
   format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
   format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
-  format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2) + 9);
   format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
   format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
 
